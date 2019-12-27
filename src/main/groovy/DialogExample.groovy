b81ffcb44adb9e08d0303ae102a58938ab2e6f9c
javatechs <javatechs@gmail.com>
2019-12-27 11:20:55 -0800

    Test code

--- src/main/groovy/DialogExample.groovy
+++ src/main/groovy/DialogExample.groovy
@@ -1,5 +1,6 @@
 // Example ad hoc dialog
 import javax.swing.*
+import com.neuronrobotics.sdk.util.ThreadUtil;
 
 def prompt = {
   JFrame jframe = new JFrame()
@@ -11,3 +12,9 @@ def prompt = {
 def first = prompt("Enter a number")
 def second = prompt("Enter another number")
 System.out.println(first + second)
+if(args!=null) {
+//	println args
+	println "Got here!"
+	ThreadUtil.wait(1500)
+	println args
+}
